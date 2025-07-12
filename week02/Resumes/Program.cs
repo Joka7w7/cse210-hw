ff1062e529f44c0d144b1e969f269bf58056b2ff
Jose Chanax <chanaxjose@gmail.com>
2025-07-12 12:06:55 -0600

    Add Job and Resume classes with functionality to display job details

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Telus";
+        job1._startYear = 2024;
+        job1._endYear = 2025;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Real Time Analyst";
+        job2._company = "Telus";
+        job2._startYear = 2023;
+        job2._endYear = 2024;
+
+        Resume myResume = new Resume();
+        myResume._name = "Jose Chanax";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
